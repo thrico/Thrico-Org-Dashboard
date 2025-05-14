e9c899e9d01f4a724508f009fd9cc5267897bc0c
Pankaj PulsePlay <pulseplay@Pankajs-MacBook-Pro.local>
2025-05-14 12:58:15 +0530

    added

--- apps/admin/utils/withAuth.tsx
+++ apps/admin/utils/withAuth.tsx
@@ -8,10 +8,10 @@ export default (WrappedComponent: any, options = { ssr: false }) => {
   function WithAuth(props: any) {
     const ACCOUNTS_URL = process.env.ACCOUNTS_URL
       ? process.env.ACCOUNTS_URL
-      : "https://accounts.thrico.com/login";
+      : "https://accounts.thrico.com";
     const DASHBOARD_URL = process.env.NEXT_PUBLIC_DASHBOARD_URL
       ? process.env.NEXT_PUBLIC_DASHBOARD_URL
-      : "https://dashboard.thrico.com/";
+      : "https://dashboard.thrico.com";
     const pathname = usePathname();
 
     const { data: { getUser } = {}, loading, error } = getGetUser();
